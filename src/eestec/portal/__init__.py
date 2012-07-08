b22a23b0e3cedb9b89a34621083bfa8e6a892efa
Nejc Zupan <nejc.zupan@gmail.com>
2012-07-09 00:39:36 +0200

    set suffixes we will use for group IDs

--- src/eestec/portal/__init__.py
+++ src/eestec/portal/__init__.py
@@ -5,6 +5,10 @@ from zope.i18nmessageid import MessageFactory
 
 _ = MessageFactory('eestec.portal')
 
+LC_MEMBERS_GROUP_SUFFIX = '-members'
+LC_BOARD_GROUP_SUFFIX = '-board'
+LC_NOTIFICATIONS_GROUP_SUFFIX = '-notifications'
+
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
