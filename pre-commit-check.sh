a6db33d069a4e2f395c955a328a7016df3bebc29
Nejc Zupan <nejc.zupan@gmail.com>
2012-07-08 19:00:03 +0200

    also run ZPTLint for .xml and .zcml files

--- pre-commit-check.sh
+++ pre-commit-check.sh
@@ -5,6 +5,8 @@ bin/test -s eestec.portal
 
 echo '====== Running ZPTLint ======'
 for pt in `find src/eestec/portal/ -name "*.pt"` ; do bin/zptlint $pt; done
+for xml in `find src/eestec/portal/ -name "*.xml"` ; do bin/zptlint $xml; done
+for zcml in `find src/eestec/portal/ -name "*.zcml"` ; do bin/zptlint $zcml; done
 
 echo '====== Running PyFlakes ======'
 bin/zopepy setup.py flakes
