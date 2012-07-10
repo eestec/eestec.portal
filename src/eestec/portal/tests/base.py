a4f7309503a7c7b744922f0568bad9aabb8ac291
Domen Kožar <domen@dev.si>
2012-07-10 20:02:15 +0200

    use PloneWithPackageLayer

--- src/eestec/portal/tests/base.py
+++ src/eestec/portal/tests/base.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """Base module for unit-testing."""
+import unittest2 as unittest
 
 from plone import api
 from plone.app.testing import applyProfile
@@ -7,7 +8,7 @@ from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import login
 from plone.app.testing import PLONE_FIXTURE
-from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import PloneWithPackageLayer
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import TEST_USER_NAME
@@ -16,24 +17,14 @@ from plone.testing import z2
 from Products.CMFPlone.tests.utils import MockMailHost
 from Products.MailHost.interfaces import IMailHost
 
-import unittest2 as unittest
+import eestec.portal
 
 
-class EestecPortalLayer(PloneSandboxLayer):
-
+class EestecPortalLayer(PloneWithPackageLayer):
     defaultBases = (PLONE_FIXTURE,)
 
-    def setUpZope(self, app, configurationContext):
-        """Set up Zope."""
-        # Load ZCML
-        import eestec.portal
-        self.loadZCML(package=eestec.portal)
-        z2.installProduct(app, 'eestec.portal')
-
     def setUpPloneSite(self, portal):
-        """Set up Plone."""
-        # Install into Plone site using portal_setup
-        applyProfile(portal, 'eestec.portal:default')
+        super(EestecPortalLayer, self).setUpPloneSite(portal)
 
         # Mock MailHost
         mockmailhost = MockMailHost('MailHost')
@@ -52,14 +43,17 @@ class EestecPortalLayer(PloneSandboxLayer):
 
     def tearDownZope(self, app):
         """Tear down Zope."""
-        z2.uninstallProduct(app, 'eestec.portal')
+        z2.uninstallProduct(app, 'eestec.portal')  # TODO: move to plone.app.testing
 
 
-FIXTURE = EestecPortalLayer()
-INTEGRATION_TESTING = IntegrationTesting(
-    bases=(FIXTURE,), name="EestecPortalLayer:Integration")
-FUNCTIONAL_TESTING = FunctionalTesting(
-    bases=(FIXTURE,), name="EestecPortalLayer:Functional")
+FIXTURE = EestecPortalLayer(zcml_filename='configure.zcml',
+                            zcml_package=eestec.portal,
+                            gs_profile_id='eestec.portal:default',
+                            additional_z2_products=('eestec.portal',))
+INTEGRATION_TESTING = IntegrationTesting(bases=(FIXTURE,),
+                                         name="EestecPortalLayer:Integration")
+FUNCTIONAL_TESTING = FunctionalTesting(bases=(FIXTURE,),
+                                       name="EestecPortalLayer:Functional")
 
 
 class IntegrationTestCase(unittest.TestCase):
