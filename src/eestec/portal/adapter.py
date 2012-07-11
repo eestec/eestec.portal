a38a68ace5bd58fe9d5ad3ae13bd5ba422916fb9
Chris Perivolaropoulos <darksaga2006@gmail.com>
2012-07-12 02:07:15 +0300

    LC drop down during user creation

--- src/eestec/portal/adapter.py
+++ src/eestec/portal/adapter.py
@@ -8,80 +8,80 @@ class EestecPortalUserDataPanelAdapter(UserDataPanelAdapter):
         return self.context.setMemberProperties({'lc': value})
     lc = property(get_lc, set_lc)
 
-    # def get_mobile(self):
-    #     return self.context.getProperty('mobile', '')
-    # def set_mobile(self, value):
-    #     return self.context.setMemberProperties({'mobile': value})
-    # mobile = property(get_mobile, set_mobile)
+    def get_mobile(self):
+        return self.context.getProperty('mobile', '')
+    def set_mobile(self, value):
+        return self.context.setMemberProperties({'mobile': value})
+    mobile = property(get_mobile, set_mobile)
 
-    # def get_study_field(self):
-    #     return self.context.getProperty('study_field', '')
-    # def set_study_field(self, value):
-    #     return self.context.setMemberProperties({'study_field': value})
-    # study_field = property(get_study_field, set_study_field)
+    def get_study_field(self):
+        return self.context.getProperty('study_field', '')
+    def set_study_field(self, value):
+        return self.context.setMemberProperties({'study_field': value})
+    study_field = property(get_study_field, set_study_field)
 
-    # def get_birthdate(self):
-    #     return self.context.getProperty('birthdate', '')
-    # def set_birthdate(self, value):
-    #     return self.context.setMemberProperties({'birthdate': value})
-    # birthdate = property(get_birthdate, set_birthdate)
+    def get_birthdate(self):
+        return self.context.getProperty('birthdate', '')
+    def set_birthdate(self, value):
+        return self.context.setMemberProperties({'birthdate': value})
+    birthdate = property(get_birthdate, set_birthdate)
 
-    # def get_sex(self):
-    #     return self.context.getProperty('sex', '')
-    # def set_sex(self, value):
-    #     return self.context.setMemberProperties({'sex': value})
-    # sex = property(get_sex, set_sex)
+    def get_sex(self):
+        return self.context.getProperty('sex', '')
+    def set_sex(self, value):
+        return self.context.setMemberProperties({'sex': value})
+    sex = property(get_sex, set_sex)
 
-    # def get_nationality(self):
-    #     return self.context.getProperty('nationality', '')
-    # def set_nationality(self, value):
-    #     return self.context.setMemberProperties({'nationality': value})
-    # nationality = property(get_nationality, set_nationality)
+    def get_nationality(self):
+        return self.context.getProperty('nationality', '')
+    def set_nationality(self, value):
+        return self.context.setMemberProperties({'nationality': value})
+    nationality = property(get_nationality, set_nationality)
 
-    # def get_address(self):
-    #     return self.context.getProperty('address', '')
-    # def set_address(self, value):
-    #     return self.context.setMemberProperties({'address': value})
-    # address = property(get_address, set_address)
+    def get_address(self):
+        return self.context.getProperty('address', '')
+    def set_address(self, value):
+        return self.context.setMemberProperties({'address': value})
+    address = property(get_address, set_address)
 
-    # def get_passport_id(self):
-    #     return self.context.getProperty('passport_id', '')
-    # def set_passport_id(self, value):
-    #     return self.context.setMemberProperties({'passport_id': value})
-    # passport_id = property(get_passport_id, set_passport_id)
+    def get_passport_id(self):
+        return self.context.getProperty('passport_id', '')
+    def set_passport_id(self, value):
+        return self.context.setMemberProperties({'passport_id': value})
+    passport_id = property(get_passport_id, set_passport_id)
 
-    # def get_passport_date_of_issue(self):
-    #     return self.context.getProperty('passport_date_of_issue', '')
-    # def set_passport_date_of_issue(self, value):
-    #     return self.context.setMemberProperties({'passport_date_of_issue': value})
-    # passport_date_of_issue = property(get_passport_date_of_issue, set_passport_date_of_issue)
+    def get_passport_date_of_issue(self):
+        return self.context.getProperty('passport_date_of_issue', '')
+    def set_passport_date_of_issue(self, value):
+        return self.context.setMemberProperties({'passport_date_of_issue': value})
+    passport_date_of_issue = property(get_passport_date_of_issue, set_passport_date_of_issue)
 
-    # def get_passport_valid_until(self):
-    #     return self.context.getProperty('passport_valid_until', '')
-    # def set_passport_valid_until(self, value):
-    #     return self.context.setMemberProperties({'passport_valid_until': value})
-    # passport_valid_until = property(get_passport_valid_until, set_passport_valid_until)
+    def get_passport_valid_until(self):
+        return self.context.getProperty('passport_valid_until', '')
+    def set_passport_valid_until(self, value):
+        return self.context.setMemberProperties({'passport_valid_until': value})
+    passport_valid_until = property(get_passport_valid_until, set_passport_valid_until)
 
-    # def get_tshirt_size(self):
-    #     return self.context.getProperty('tshirt_size', '')
-    # def set_tshirt_size(self, value):
-    #     return self.context.setMemberProperties({'tshirt_size': value})
-    # tshirt_size = property(get_tshirt_size, set_tshirt_size)
+    def get_tshirt_size(self):
+        return self.context.getProperty('tshirt_size', '')
+    def set_tshirt_size(self, value):
+        return self.context.setMemberProperties({'tshirt_size': value})
+    tshirt_size = property(get_tshirt_size, set_tshirt_size)
 
-    # def get_needs(self):
-    #     return self.context.getProperty('needs', '')
-    # def set_needs(self, value):
-    #     return self.context.setMemberProperties({'needs': value})
-    # needs = property(get_needs, set_needs)
+    def get_needs(self):
+        return self.context.getProperty('needs', '')
+    def set_needs(self, value):
+        return self.context.setMemberProperties({'needs': value})
+    needs = property(get_needs, set_needs)
 
-    # def get_captcha(self):
-    #     return self.context.getProperty('captcha', '')
-    # def set_captcha(self, value):
-    #     return self.context.setMemberProperties({'captcha': value})
-    # captcha = property(get_captcha, set_captcha)
+    def get_captcha(self):
+        return self.context.getProperty('captcha', '')
+    def set_captcha(self, value):
+        return self.context.setMemberProperties({'captcha': value})
+    captcha = property(get_captcha, set_captcha)
 
-    # def get_warning(self):
-    #     return self.context.getProperty('warning', '')
-    # def set_warning(self, value):
-    #     return self.context.setMemberProperties({'warning': value})
-    # warning = property(get_warning, set_warning)
+    def get_warning(self):
+        return self.context.getProperty('warning', '')
+    def set_warning(self, value):
+        return self.context.setMemberProperties({'warning': value})
+    warning = property(get_warning, set_warning)
--- src/eestec/portal/content/userdata.py
+++ src/eestec/portal/content/userdata.py
@@ -6,168 +6,189 @@ from zope import schema
 # from collective.examples.userdata import _
 from plone.app.users.userdataschema import IUserDataSchemaProvider
 from plone.app.users.userdataschema import IUserDataSchema
+from plone import api
+
+from Products.CMFCore.utils import getToolByName
+from zope.component.hooks import getSite
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+from zope.interface import implements
+
 
 STUDY_FIELDS = ['ece', 'electrical engineering', 'engineering of electics', 'electrical']
 SEXES = ['male', 'female']
 COUNTRIES = ['greece' , 'USA', 'other countries']
 TSHIRT_SIZES = ['Large', 'Medium', 'Gay']
 
-class MemberValidator(object):
-    """Information about an EESTEC member."""
-    def __init__(self, study_fields, countries, tshirt_sizes, sexes):
-        self.study_fields = study_fields
-        self.countries = countries
-        self.tshirt_sizes = tshirt_sizes
-        self.sexes = sexes
+class PortalTypesVocabulary(object):
 
-    def LCs(self, val):
-        """Return all registered LCs on the webpage."""
-        portal_catalog = getToolByName(self, 'portal_catalog')
+    implements(IVocabularyFactory)
 
-        brains = portal_catalog(Type='LC', sort_on='sortable_title')
+    def __init__(self, portal_type):
+        self.portal_type = portal_type
 
-        lcs = []
-        for brain in brains:
-            lcs.append((brain.getObject().getId(),
-                        brain.getObject().getFull_lc_title()))
-        return val in lcs
+    def __call__(self, context):
+        site = getSite()
+        catalog = getToolByName(site, 'portal_catalog', None)
+        if catalog is None:
+            return SimpleVocabulary([])
 
-    def current_LC(self, val):
-        """Returns an object of the LC this EestecMember is a member of."""
-        catalog = getToolByName(self, 'portal_catalog')
-        return val in catalog(Type='LC', id=self.lc)[0].getObject()
+        brains = catalog(portal_type=self.portal_type)
+        items = [(brain.UID, brain.Title) for brain in brains]
+        # Most schema fields expect unicode values.
+        terms = [SimpleTerm(value=unicode(pair[0]), token=unicode(pair[0]),
+                             title=pair[1]) for pair in items]
+        return SimpleVocabulary(terms)
 
-    def study_fields(self, val):
+
+    # FIXME: code
+    # def LC_list():
+        # return ["LC1" "LC2"]
+        # portal = api.portal.get()
+        # if not portal:
+        #     import ipdb; ipdb.set_trace()
+        #     return ["portal said fuck you", "but I think it meant your mother"]
+
+        # return [lc for lc in portal["lc"] if lc.type == "eestec.portal.lc"]
+
+
+# Pick the portal_type that you want here:
+PFGFormsVocabularyFactory = PortalTypesVocabulary('eestec.portal.lc')
+
+
+class Validate:
+    def study_fields(val):
         """Return all study fields that are specified in config.py."""
-        return val in self.study_fields
+        return val in STUDY_FIELDS
 
     def sex(self):
         """Return options for the 'sex' drop-down menu."""
-        return val in self.sexes
+        return val in SEXES
 
-    def country(self, val):
+    def country(val):
         """Return all countries that are specified in config.py."""
-        return val in self.countries
+        return val in COUNTRIES
 
-    def sizes(self, val):
+    def sizes(val):
         """Return all tshirt sizes that are specified in config.py."""
-        return self.tshirt_sizes
-
+        return TSHIRT_SIZES
 
 ### FIXME: The current date around wich dates are set is the day that
 ### the server started, not tha day of submission
 class IEestecPortalUserDataSchema(IUserDataSchema):
     """Use all the fields from the default user data schema, and add
-    various extra fields.
-    """
-    # _validator = MemberValidator(STUDY_FIELDS, COUNTRIES, TSHIRT_SIZES, SEXES)
-    lc = schema.TextLine(
+    various extra fields."""
+
+    lc = schema.Choice(
         title = u"LC",
-        #validate = _validator.LCs,
+        #values = LC_list(),
+        vocabulary = "lc_list",
         required=True,
         description=u"Select the Local Committee you belong to. If you \
         don't know what this is just leave it at \
         'Observer Candidates'."
         )
 
-#     mobile = schema.TextLine(
-#         title = _( u'mobile', default = u"Mobile"),
-#         description="Enter the mobile number on which you are \
-#         available for the organizers.",
-#         # read_permission=ModifyPortalContent,
-#         )
+    mobile = schema.TextLine(
+        title = u'Mobile',
+        description=u"Enter the mobile number on which you are \
+        available for the organizers.",
+        # read_permission=ModifyPortalContent,
+        )
 
-#     study_field = schema.TextLine(
-#         title = _( u'study_field', default = u"Study Field"),
-#         #validate=_validator.study_fields,
-#         description="Select your study field.",
-#         format='select',
-#         )
+    study_field = schema.Choice(
+        title = u'Study Field',
+        constraint=Validate.study_fields,
+        description=u"Select your study field.",
+        values = STUDY_FIELDS,
+        )
 
-#     birthdate = schema.Date(
-#         title = _( u'birthdate', default = u"Birthdate"),
-#         description="Select your birthdate.",
-#         max = date.today(),
-#         min = date(1964, 1, 1),
-#         # read_permission=ModifyPortalContent,
-#         )
+    birthdate = schema.Date(
+        title = u'Birthdate',
+        description=u"Select your birthdate.",
+        max = date.today(),
+        min = date(1964, 1, 1),
+        # read_permission=ModifyPortalContent,
+        )
 
-#     sex = schema.Choice(
-#         title = _( u'sex', default = u"Sex"),
-#         validate=_validator.sex,
-#         description="Select your sex.",
-#         )
+    sex = schema.Choice(
+        title = u'Sex',
+        constraint=Validate.sex,
+        description=u"Select your sex.",
+        values = SEXES,
+        )
 
-#     nationality = schema.Choice(
-#         title = _( u'nationality', default = u"Nationality"),
-#         validate=_validator.countries,
-#         description="Passport Details",
-#         values = NA
-#         )
+    nationality = schema.Choice(
+        title = u'Nationality',
+        constraint=Validate.country,
+        description=u"Passport Details",
+        values = COUNTRIES
+        )
 
-#     address = schema.TextLine(
-#         title = _( u'address', default = u"Address"),
-#         description="Passport Details",
-#         # read_permission=ModifyPortalContent,
-#         )
+    address = schema.TextLine(
+        title = u'Address',
+        description=u"Passport Details",
+        # read_permission=ModifyPortalContent,
+        )
 
-#     passport_id = schema.TextLine(
-#         title = _( u'passport_id', default = u"Passport ID Number"),
-#         description="Passport Details",
-#         # read_permission=ModifyPortalContent,
-#         )
+    passport_id = schema.TextLine(
+        title = u'Passport ID',
+        description=u"Passport Details",
+        # read_permission=ModifyPortalContent,
+        )
 
-#     passport_date_of_issue = schema.Date(
-#         title = _( u'passport_date_of_issue', default = u"Date Of Issue"),
-#         description="Passport Details",
-#         min = date.today().replace(year=date.today().year - 10),
-#         max = date.today(),
-#         # read_permission=ModifyPortalContent,
-#         )
+    passport_date_of_issue = schema.Date(
+        title = u'Passport date of issue',
+        description=u"Passport Details",
+        min = date.today().replace(year=date.today().year - 10),
+        max = date.today(),
+        # read_permission=ModifyPortalContent,
+        )
 
-#     passport_valid_until = schema.Date(
-#         title = _( u'passport_valid_until', default = u"Valid Until"),
-#         description="Passport Details",
-#         max = date.today().replace(year=date.today().year + 10),
-#         min = date.today(),
-#         # read_permission=ModifyPortalContent,
-#         )
+    passport_valid_until = schema.Date(
+        title = u'Passport valid until',
+        description=u"Passport Details",
+        max = date.today().replace(year=date.today().year + 10),
+        min = date.today(),
+        # read_permission=ModifyPortalContent,
+        )
 
-#     tshirt_size = schema.Choice(
-#         title = _( u'tshirt_size', default = u"T-Shirt size"),
-#         validate=_validator.sizes,
-#         description="Select your T-Shirt size.",
-#         values = TSHIRT_SIZES,
-#         # read_permission=ModifyPortalContent,
-#         )
+    tshirt_size = schema.Choice(
+        title = u'T-Shirt size',
+        constraint=Validate.sizes,
+        description=u"Select your T-Shirt size.",
+        values = TSHIRT_SIZES,
+        # read_permission=ModifyPortalContent,
+        )
 
-#     needs = schema.TextField(
-#         title = _( u'needs', default = u"Special needs"),
-#         description="Any special needs? Food allergies? Medical \
-#         issues? Anything the organizers need to be \
-#         aware of? Anything the organizers need to \
-#         tell us?",
-#         # read_permission=ModifyPortalContent,
-#         )
+    needs = schema.Text(
+        title = u'Special needs',
+        description=u"Any special needs? Food allergies? Medical \
+        issues? Anything the organizers need to be \
+        aware of? Anything the organizers need to \
+        tell us?",
+        # read_permission=ModifyPortalContent,
+        )
 
-#     captcha = schema.Int(
-#         title = _( u'captcha', default = ustr(n1) + " + " + str(n2) + " is: "),
-#         description="Captcha",
-#         # read_permission=ModifyPortalContent,
-#         required=True,
-#         validator=_validator.range
-#         )
+    # FIXME: Add image and everything
+    captcha = schema.Int(
+        title = u'Captcha',
+        description=u"Captcha",
+        # read_permission=ModifyPortalContent,
+        required=True,
+        )
 
-#     warning = schema.TextLine(
-#         title = _( u'warning', default = u"Warning!"),
-#         description="In case you are applying for an Event \
-#         for the first time, the system redirected you \
-#         to this page (your profile page) so you enter \
-#         all necessary information. Once you click 'save' \
-#         your personal information will be saved. Then you \
-#         need to GO BACK TO THE EVENT PAGE AND CLICK APPLY \
-#         AGAIN!"
-# )
+    # FIXME: displays an inputline
+    warning = schema.TextLine(
+        title = u'Warning',
+        description=u"In case you are applying for an Event \
+        for the first time, the system redirected you \
+        to this page (your profile page) so you enter \
+        all necessary information. Once you click 'save' \
+        your personal information will be saved. Then you \
+        need to GO BACK TO THE EVENT PAGE AND CLICK APPLY \
+        AGAIN!"
+        )
 
 
 class UserDataSchemaProvider(object):
