d137f45f2e31ae65f86c077c8ee78ca949666f59
Vilmos Somogyi <vsomogyi@gmail.com>
2012-07-12 20:16:28 +0200

    mobile version fixes

--- src/eestec/portal/browser/theme/javascript/script.js
+++ src/eestec/portal/browser/theme/javascript/script.js
@@ -1,11 +1,13 @@
 (function ($) {
     $(function () {
         $('#mobile-globalnav').bind('click', function() {
+            $(this).toggleClass('mobile-menu-opened');
             $('#portal-globalnav').toggleClass('visible');
             $('#portal-searchbox').removeClass('visible');
         });
     
         $('#mobile-search').bind('click', function() {
+            $(this).toggleClass('mobile-menu-opened');
             $('#portal-searchbox').toggleClass('visible');
             $('#portal-globalnav').removeClass('visible');
         });
