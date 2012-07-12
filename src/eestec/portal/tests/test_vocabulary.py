ff7e3288e611008de5612c8cf50932aef80102c0
Chris Perivolaropoulos <darksaga2006@gmail.com>
2012-07-12 22:27:40 +0300

    Added testing of lc_list vocabulary

---
+++ src/eestec/portal/tests/test_vocabulary.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from datetime import date
+
+from eestec.portal.tests.base import IntegrationTestCase
+from plone import api
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
+
+import unittest2 as unittest
+
+class TestLCListVocabulary(IntegrationTestCase):
+    """Test the vocabulary that renturns a list of LCs
+    """
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+        api.content.create(
+            type='eestec.portal.lc',
+            title=u'patra',
+            container=self.portal
+        )
+
+        api.content.create(
+            type='eestec.portal.lc',
+            title=u'athens',
+            container=self.portal
+        )
+        factory = getUtility(IVocabularyFactory, 'lc_list')
+        self.vocabulary = factory(self.portal)
+
+    def test_returned_list(self):
+        self.assertEquals( [u'patra', u'athens'], [i.title for i in self.vocabulary])
