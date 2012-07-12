0c4d0ea05f10dbaad89d6802e78619a6a75f7d3b
Nejc Zupan <nejc.zupan@gmail.com>
2012-07-12 23:32:50 +0200

    a big refactor and cleanup of userdata.py and its tests

--- src/eestec/portal/content/user.py
+++ src/eestec/portal/content/user.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
+"""User object. We use the default Plone user objects of the MemberData class
+and just extend/override some stuff with eestec.portal specifics.
+"""
 
+from plone import api
 from datetime import date
-
-from zope.interface import implements
-from zope import schema
-
-from plone.app.users.userdataschema import IUserDataSchemaProvider
+from plone.app.users.browser.personalpreferences import UserDataConfiglet
+from plone.app.users.browser.personalpreferences import UserDataPanel
+from plone.app.users.browser.personalpreferences import UserDataPanelAdapter
 from plone.app.users.userdataschema import IUserDataSchema
-
+from plone.app.users.userdataschema import IUserDataSchemaProvider
+from zope import schema
+from zope.interface import implements
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
@@ -18,18 +22,15 @@ COUNTRIES = ['greece', 'USA', 'other countries']
 TSHIRT_SIZES = ['Large', 'Medium', 'Small']
 
 
-class PortalTypesVocabulary(object):
-    """Will return a list of all LCs"""
+class LCVocabulary(object):
+    """Will return a list of all LCs."""
 
     implements(IVocabularyFactory)
-
-    def __init__(self, portal_type):
-        self.portal_type = portal_type
 
     def __call__(self, context):
         terms = []
 
-        lc_folder = context.lc  # context == portal
+        lc_folder = api.portal.get().lc
         for lc_id, lc in lc_folder.items():
 
             # only add LC types as vocabulary terms
@@ -48,7 +49,7 @@ class PortalTypesVocabulary(object):
 
 
 # Pick the portal_type that you want here:
-PFGFormsVocabularyFactory = PortalTypesVocabulary('eestec.portal.lc')
+LCVocabularyFactory = LCVocabulary()
 
 
 class Validate:
@@ -186,6 +187,108 @@ class IEestecPortalUserDataSchema(IUserDataSchema):
     )
 
 
+class EestecPortalUserDataPanelAdapter(UserDataPanelAdapter):
+    """User adapter."""
+
+    def get_lc(self):
+        return self.context.getProperty('lc', '')
+
+    def set_lc(self, value):
+        return self.context.setMemberProperties({'lc': value})
+    lc = property(get_lc, set_lc)
+
+    def get_mobile(self):
+        return self.context.getProperty('mobile', '')
+
+    def set_mobile(self, value):
+        return self.context.setMemberProperties({'mobile': value})
+    mobile = property(get_mobile, set_mobile)
+
+    def get_study_field(self):
+        return self.context.getProperty('study_field', '')
+
+    def set_study_field(self, value):
+        return self.context.setMemberProperties({'study_field': value})
+    study_field = property(get_study_field, set_study_field)
+
+    def get_birthdate(self):
+        return self.context.getProperty('birthdate', '')
+
+    def set_birthdate(self, value):
+        return self.context.setMemberProperties({'birthdate': value})
+    birthdate = property(get_birthdate, set_birthdate)
+
+    def get_sex(self):
+        return self.context.getProperty('sex', '')
+
+    def set_sex(self, value):
+        return self.context.setMemberProperties({'sex': value})
+    sex = property(get_sex, set_sex)
+
+    def get_nationality(self):
+        return self.context.getProperty('nationality', '')
+
+    def set_nationality(self, value):
+        return self.context.setMemberProperties({'nationality': value})
+    nationality = property(get_nationality, set_nationality)
+
+    def get_address(self):
+        return self.context.getProperty('address', '')
+
+    def set_address(self, value):
+        return self.context.setMemberProperties({'address': value})
+    address = property(get_address, set_address)
+
+    def get_passport_id(self):
+        return self.context.getProperty('passport_id', '')
+
+    def set_passport_id(self, value):
+        return self.context.setMemberProperties({'passport_id': value})
+    passport_id = property(get_passport_id, set_passport_id)
+
+    def get_passport_date_of_issue(self):
+        return self.context.getProperty('passport_date_of_issue', '')
+
+    def set_passport_date_of_issue(self, value):
+        return self.context.setMemberProperties({'passport_date_of_issue': value})
+    passport_date_of_issue = property(get_passport_date_of_issue, set_passport_date_of_issue)
+
+    def get_passport_valid_until(self):
+        return self.context.getProperty('passport_valid_until', '')
+
+    def set_passport_valid_until(self, value):
+        return self.context.setMemberProperties({'passport_valid_until': value})
+    passport_valid_until = property(get_passport_valid_until, set_passport_valid_until)
+
+    def get_tshirt_size(self):
+        return self.context.getProperty('tshirt_size', '')
+
+    def set_tshirt_size(self, value):
+        return self.context.setMemberProperties({'tshirt_size': value})
+    tshirt_size = property(get_tshirt_size, set_tshirt_size)
+
+    def get_needs(self):
+        return self.context.getProperty('needs', '')
+
+    def set_needs(self, value):
+        return self.context.setMemberProperties({'needs': value})
+    needs = property(get_needs, set_needs)
+
+    def get_captcha(self):
+        return self.context.getProperty('captcha', '')
+
+    def set_captcha(self, value):
+        return self.context.setMemberProperties({'captcha': value})
+    captcha = property(get_captcha, set_captcha)
+
+    def get_warning(self):
+        return self.context.getProperty('warning', '')
+
+    def set_warning(self, value):
+        return self.context.setMemberProperties({'warning': value})
+    warning = property(get_warning, set_warning)
+
+
 class UserDataSchemaProvider(object):
     implements(IUserDataSchemaProvider)
 
@@ -193,3 +296,33 @@ class UserDataSchemaProvider(object):
         """
         """
         return IEestecPortalUserDataSchema
+
+
+class EestecPortalUserDataConfiglet(UserDataConfiglet):
+    """Customize @@user-information view."""
+
+    def __call__(self):
+        # Omit default Plone fields
+        self.form_fields = self.form_fields.omit(
+            'home_page',
+            'description',
+            'location',
+            'pdelete',
+        )
+
+        return super(EestecPortalUserDataConfiglet, self).__call__()
+
+
+class PLRUserDataPanel(UserDataPanel):
+    """Customize @@personal-information view."""
+
+    def __call__(self):
+        # Omit Plone default fields
+        self.form_fields = self.form_fields.omit(
+            'home_page',
+            'description',
+            'location',
+            'pdelete',
+        )
+
+        return super(PLRUserDataPanel, self).__call__()
--- src/eestec/portal/tests/test_user.py
+++ src/eestec/portal/tests/test_user.py
@@ -1,66 +1,62 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from datetime import date
 
+from datetime import date
 from eestec.portal.tests.base import IntegrationTestCase
 from plone import api
 
 
-class TestUser(IntegrationTestCase):
+class TestUserFields(IntegrationTestCase):
     """Test eestec User
     """
 
     def setUp(self):
+        """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
-        self.workflow = api.portal.get_tool('portal_workflow')
+        self.request = self.layer['request']
 
-        self.user = api.user.create(email='lois@plone.org', username='lois', properties=dict(
-                                    lc=u'Quahog',
-                                    mobile=u'666',
-                                    study_field=u'housewife',
-                                    birthdate=date(1960, 7, 22),
-                                    sex=u'Female',
-                                    nationality=u'USA',
-                                    address=u'Rhode Island',
-                                    passport_id=u'666',
-                                    passport_date_of_issue=date.today(),
-                                    passport_valid_until=date.today(),
-                                    tshirt_size=u'Large',
-                                    needs=u'No needs'
-                                    ))
+    def test_adapter_registration(self):
+        """Test that self.portal is adapted by IEestecPortalUserDataSchema."""
+        from eestec.portal.content.user import IEestecPortalUserDataSchema
+        adapter = IEestecPortalUserDataSchema(self.portal)
+        self.assertNotEquals(adapter, None)
 
-    def test_lc(self):
+    def test_fields_set_upon_user_creation(self):
+        """Test that fields are correctly set when a user is created."""
+        self.user = api.user.create(
+            email='lois@plone.org',
+            username='lois',
+            properties=dict(
+                lc=u'Quahog',
+                mobile=u'666',
+                study_field=u'housewife',
+                birthdate=date(1960, 7, 22),
+                sex=u'Female',
+                nationality=u'USA',
+                address=u'Rhode Island',
+                passport_id=u'666',
+                passport_date_of_issue=date.today(),
+                passport_valid_until=date.today(),
+                tshirt_size=u'Large',
+                needs=u'No needs'
+            )
+        )
+
         self.assertEquals(self.user.getProperty('lc'), u'Quahog')
-
-    def test_mobile(self):
         self.assertEquals(self.user.getProperty('mobile'), u'666')
-
-    def test_study_field(self):
         self.assertEquals(self.user.getProperty('study_field'), u'housewife')
-
-    def test_birthdate(self):
         self.assertEquals(self.user.getProperty('birthdate'), date(1960, 7, 22))
-
-    def test_sex(self):
         self.assertEquals(self.user.getProperty('sex'), u'Female')
-
-    def test_nationality(self):
         self.assertEquals(self.user.getProperty('nationality'), u'USA')
-
-    def test_address(self):
         self.assertEquals(self.user.getProperty('address'), u'Rhode Island')
-
-    def test_passport_id(self):
         self.assertEquals(self.user.getProperty('passport_id'), u'666')
-
-    def test_passport_date_of_issue(self):
-        self.assertEquals(self.user.getProperty('passport_date_of_issue'), date.today())
-
-    def test_passport_valid_until(self):
-        self.assertEquals(self.user.getProperty('passport_valid_until'), date.today())
-
-    def test_tshirt_size(self):
         self.assertEquals(self.user.getProperty('tshirt_size'), u'Large')
-
-    def test_needs(self):
         self.assertEquals(self.user.getProperty('needs'), u'No needs')
+        self.assertEquals(
+            self.user.getProperty('passport_date_of_issue'),
+            date.today()
+        )
+        self.assertEquals(
+            self.user.getProperty('passport_valid_until'),
+            date.today()
+        )
