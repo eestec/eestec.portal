b36bb3af00375f62f9587570ad05d08d50cead38
Nejc Zupan <nejc.zupan@gmail.com>
2012-07-12 18:00:43 +0200

    Simplify zope.event handling
    This change greatly simplifies how we handle zope.event events. Before, we
    needed to add a new subscriber to `events.zcml`, and a handler method to
    `events.py`. Consequently, the `events.py` was a toilet for all event handlers
    and could grow to be very unreadable. What we do from now on is add event
    handlers into the `content/` folder, into the file of the content item the event
    is dealing with. Moreover, we replace the use of `events.zcml` for registering
    event subscribers to using `grok.subscribe` method decorator. So no *ZCML*,
    everything is done in one file, with python.

--- src/eestec/portal/content/newsitem.py
+++ src/eestec/portal/content/newsitem.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
-"""Catch miscelaneous events and act accordingly."""
+"""News Item content type. Since we are using the default ATNewsItem type
+that comes with Plone we don't really have to do anything here to have it.
+So we only have zope.event subscribers in this file.
+"""
 
 from eestec.portal import emails
+from five import grok
+from Products.ATContentTypes.atct import ATNewsItem
+from Products.DCWorkflow.interfaces import IAfterTransitionEvent
 
 
+@grok.subscribe(ATNewsItem, IAfterTransitionEvent)
 def news_item_published(context, event):
     """When a News Item is published, send an email to the CP list."""
 
