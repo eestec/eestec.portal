4a358821333c02890fe33922a03488764936a304
Nejc Zupan <nejc.zupan@gmail.com>
2012-11-10 01:14:22 +0100

    handle exit of scripts in Travis

--- pre-commit-check.sh
+++ pre-commit-check.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+function handle_exit {
+    if [ $? -ne 0 ]; then
+        EXITCODE=1
+    fi
+}
+
+EXITCODE=0
+
 echo 'Running tests'
-bin/test
+bin/test; handle_exit
 
 echo '====== Running ZPTLint ======'
 for pt in `find src/eestec/portal/ -name "*.pt"` ; do bin/zptlint $pt; done
@@ -9,9 +17,12 @@ for xml in `find src/eestec/portal/ -name "*.xml"` ; do bin/zptlint $xml; done
 for zcml in `find src/eestec/portal/ -name "*.zcml"` ; do bin/zptlint $zcml; done
 
 echo '====== Running PyFlakes ======'
-bin/zopepy setup.py flakes
+bin/zopepy setup.py flakes; handle_exit
 
 echo '====== Running pep8 =========='
-bin/pep8 --ignore=E501 src/eestec/portal
-bin/pep8 --ignore=E501 setup.py
+bin/pep8 --ignore=E501 src/eestec/portal; handle_exit
+bin/pep8 --ignore=E501 setup.py; handle_exit
 
+if [ $EXITCODE -ne 0 ]; then
+    exit 1
+fi
