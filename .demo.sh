a36be820694f34cfddc98a4e4b219fe2c4caac3a
Nejc Zupan <nejc.zupan@gmail.com>
2012-12-09 10:59:54 +0100

    further work on getting demo content import step

---
+++ .demo.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# A cronjob script that re-creates demo.eestec.net every night
+
+git clone git://github.com/eestec/eestec.portal.git
+cd eestec.portal
+virtualenv-2.6 --no-site-packages .
+bin/python2.6 bootstrap.py
+bin/buildout -c demo.cfg
+bin/supervisord
+
+curl ...
+
