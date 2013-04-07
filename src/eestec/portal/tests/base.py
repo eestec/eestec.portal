71d539c3b549caa864626dc519b8d6f4d6677191
Nejc Zupan <nejc.zupan@gmail.com>
2013-04-07 12:09:36 +0200

    Upgrade to Plone 4.3 to keep up with latest developments in the Plone community

--- src/eestec/portal/tests/base.py
+++ src/eestec/portal/tests/base.py
@@ -4,11 +4,12 @@
 import unittest2 as unittest
 
 from plone import api
+from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import login
 from plone.app.testing import PLONE_FIXTURE
-from plone.app.testing import PloneWithPackageLayer
+from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import TEST_USER_NAME
@@ -17,19 +18,21 @@ from plone.testing import z2
 from Products.CMFPlone.tests.utils import MockMailHost
 from Products.MailHost.interfaces import IMailHost
 
-import eestec.portal
 
+class EestecPortalLayer(PloneSandboxLayer):
 
-class EestecPortalLayer(PloneWithPackageLayer):
     defaultBases = (PLONE_FIXTURE,)
 
     def setUpZope(self, app, configurationContext):
         """Set up Zope."""
-        super(EestecPortalLayer, self).setUpZope(app, configurationContext)
+        import eestec.portal
+        self.loadZCML(package=eestec.portal)
         self.loadZCML(package=eestec.portal, name='overrides.zcml')
 
     def setUpPloneSite(self, portal):
-        super(EestecPortalLayer, self).setUpPloneSite(portal)
+        """Set up Plone."""
+        # Install into Plone site using portal_setup
+        applyProfile(portal, 'eestec.portal:default')
 
         # Mock MailHost
         mockmailhost = MockMailHost('MailHost')
@@ -38,28 +41,20 @@ class EestecPortalLayer(PloneWithPackageLayer):
         sm = api.portal.get().getSiteManager()
         sm.registerUtility(component=mockmailhost, provided=IMailHost)
 
-        # Login
+        # Give TEST_USER Manager role and login
         setRoles(portal, TEST_USER_ID, ['Manager'])
         login(portal, TEST_USER_NAME)
-
-        # Rebuild the catalog and commit changes
-        portal.portal_catalog.clearFindAndRebuild()
-        import transaction
-        transaction.commit()
 
     def tearDownZope(self, app):
         """Tear down Zope."""
         z2.uninstallProduct(app, 'eestec.portal')
 
 
-FIXTURE = EestecPortalLayer(zcml_filename='configure.zcml',
-                            zcml_package=eestec.portal,
-                            gs_profile_id='eestec.portal:default',
-                            additional_z2_products=('eestec.portal',))
-INTEGRATION_TESTING = IntegrationTesting(bases=(FIXTURE,),
-                                         name="EestecPortalLayer:Integration")
-FUNCTIONAL_TESTING = FunctionalTesting(bases=(FIXTURE,),
-                                       name="EestecPortalLayer:Functional")
+FIXTURE = EestecPortalLayer()
+INTEGRATION_TESTING = IntegrationTesting(
+    bases=(FIXTURE,), name="EestecPortalLayer:Integration")
+FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(FIXTURE,), name="EestecPortalLayer:Functional")
 
 
 class IntegrationTestCase(unittest.TestCase):
